2cd5903298d86a21cc3bdbc2c2d1dd68d1610a95
Ian <iewing085@gmail.com>
2023-02-09 21:05:23 -0500

    Confirmation of vue welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -12,6 +12,8 @@ defineProps({
 <template>
     <Head title="Welcome" />
 
+    <h1>DUCKKIT</h1>
+
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
     >
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
