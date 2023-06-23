4b1be229121d7caa6ae18026eb1b2733072a8377
Ian <iewing085@gmail.com>
2023-06-23 11:22:46 -0400

    Add body to PostController

---
+++ resources/js/Components/Post.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div>{{ postTitle }}</div>
+        <div>{{ postBody }}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps(["postTitle", "postBody"]);
+</script>
+"
