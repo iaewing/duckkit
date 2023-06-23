2ded561ed937ec7c81e1cb335226364daba264f7
Ian <iewing085@gmail.com>
2023-06-23 11:22:21 -0400

    tinker with welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -44,9 +44,8 @@ defineProps({
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h1 class="bg-red-800">DUCKKIT</h1>
+                <h1 class="bg-red-800 text-white">DUCKKIT</h1>
             </div>
-
             <div
                 class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg"
             >
