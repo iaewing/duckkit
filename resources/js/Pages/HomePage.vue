efaa682ca571802f93968710ad1ea73581f81248
Ian <iewing085@gmail.com>
2023-06-29 15:13:35 -0400

    change getPosts from button to onBeforeMount

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onBeforeMount, ref } from "vue";
 import Post from "@/Components/Post.vue";
 
 const posts = ref(null);
-async function getPosts() {
+
+onBeforeMount(() => {
+    getPosts();
+})
+async function getPosts(): Promise<void> {
     try {
         await fetch('/api/posts')
             .then((response) => response.json())
@@ -11,13 +15,13 @@ async function getPosts() {
     } catch (error) {
         console.log(error);
     }
+
 }
 </script>
 
 <template>
     <div class="uppercase border-b">Duckkit</div>
     <div>
-        <button @click="getPosts">Get Posts</button>
         <div v-for="post in posts" :key="post.id">
             <Post :postTitle="post.title" :postBody="post.body" />
         </div>
