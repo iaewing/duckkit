11ad684c7fad2eb0d0503edec807eee3b5f76a6e
Ian <iewing085@gmail.com>
2023-07-06 11:59:43 -0400

    styling and adds more information about teh post

--- resources/js/Components/PostCard.vue
+++ resources/js/Components/PostCard.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { Link } from "@inertiajs/vue3";
+import { onBeforeMount } from "vue";
+import * as dayjs from "dayjs";
+import * as relativeTime from "dayjs/plugin/relativeTime";
 
 const props = defineProps(["post"]);
 const isVisible = ref(false);
+const postTime = ref(null);
+
+function calculateTime() {
+    dayjs.extend(relativeTime);
+    return dayjs(new Date(props.post.created_at).getTime()).fromNow();
+}
+
+onBeforeMount(() => {
+    postTime.value = calculateTime();
+})
 </script>
 
 <template>
-    <div class="w-1/2 border border-violet-300 my-2 px-4 py-2">
-            <Link class="text-xl text-violet-600" :href="'/post/' + post.id"  as="div">{{ post.title }}</Link>
-        <button @click="isVisible = !isVisible">Expand</button>
-        <div v-if="isVisible" class="text-md text-gray-600">{{ post.body }}</div>
+    <div class="w-3/4 my-2 px-4 py-2 flex flex-row">
+        <div class="px-2">{{ post.karma }}</div>
+        <div class="px-2">ICON</div>
+        <div class="flex flex-col">
+            <Link class="text-xl text-violet-600" :href="'/post/' + post.id" as="div">{{ post.title }}</Link>
+            <div class="flex flex-row">
+                <button @click="isVisible = !isVisible" class="pr-2">Expand</button>
+                <div>Submitted on {{ postTime }} by {{ post.user.name }} to {{ post.subduckkit.name }}</div>
+            </div>
+            <div v-if="isVisible" class="text-md text-gray-600 border border-violet-300 my-2 px-4 py-2 rounded">{{ post.body }}
+            </div>
+        </div>
     </div>
 </template>
