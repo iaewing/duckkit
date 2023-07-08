bacc61c96442ee002320fe6ca239d57f154327c4
Ian <iewing085@gmail.com>
2023-07-07 23:38:25 -0400

    create a default layout

---
+++ resources/js/Layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import DuckkitLogo from '@/Components/DuckkitLogo.vue';
+</script>
+
+<template>
+    <div class="uppercase border-b">
+        <DuckkitLogo />
+    </div>
+    <slot />
+</template>
