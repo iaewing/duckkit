da66403e756a912d524c5eaefe9780e12c333f10
Ian <iewing085@gmail.com>
2023-07-10 09:08:34 -0400

    add background to default layout

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -3,8 +3,10 @@ import DuckkitLogo from '@/Components/DuckkitLogo.vue';
 </script>
 
 <template>
+    <div class="bg-sky-50">
     <div class="uppercase border-b">
         <DuckkitLogo />
     </div>
     <slot />
+</div>
 </template>
