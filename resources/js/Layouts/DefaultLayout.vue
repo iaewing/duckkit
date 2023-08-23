ebdaa558d834dc8c87b1902c80687d6be2933889
Ian <iewing085@gmail.com>
2023-08-23 16:41:03 -0400

    add Log in and Register to default layout

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,12 +1,16 @@
 <script setup>
 import DuckkitLogo from '@/Components/DuckkitLogo.vue';
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
     <div class="bg-sky-50">
-    <div class="uppercase border-b">
-        <DuckkitLogo />
+        <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
+        <Link :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">
+        Register</Link>
+        <div class="uppercase border-b">
+            <DuckkitLogo />
+        </div>
+        <slot />
     </div>
-    <slot />
-</div>
 </template>
